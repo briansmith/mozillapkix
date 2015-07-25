c74ae055a2cef790c91d6db39012eb318e6354ab
Brian Smith <brian@briansmith.org>
2015-07-24 22:10:13 -0400

    Fix Travis CI script; temporarily disable clang-3.7 on Travis.
    Travis CI is now not installing clang-3.7 like it was even earlier
    today, so disable clang-3.7. Also update the update-travis-yml script
    to be more in sync with the version in ring.

--- mk/update-travis-yml.py
+++ mk/update-travis-yml.py
@@ -23,11 +23,14 @@ compilers = [
     # Since Travis CI limits the number of concurrent builds, we put the
     # highest-signal (most likely to break) builds first, to reduce latency
     # in discovering broken builds.
-    latest_clang, # Newest clang first.
+    #
+    # XXX TODO: clang-3.7 was available and working using this setup, but for
+    # some reason it isn't working any more as of 2015-07-24, so it has been
+    # removed from the matrix for now.
+    "clang-3.6", # Newest clang first.
     "gcc-4.8",   # Oldest GCC next.
     
     # All other clang versions, newest to oldest.
-    "clang-3.6",
     "clang-3.5",
     "clang-3.4",
 
@@ -41,20 +44,19 @@ modes = [
     "RELWITHDEBINFO"
 ]
 
-bits_choices = [
-    "64",
-    "32",
+archs = [
+    "x86",
+    "x86_64",
 ]
 
 def format_entries():
-    return "\n".join([format_entry(compiler, mode, bits)
+    return "\n".join([format_entry(compiler, mode, arch)
                       for mode in modes
-                      for bits in bits_choices
+                      for arch in archs
                       for compiler in compilers
-                      # XXX: 32-bit GCC does not work because Travis does not
-                      # have g++-4.8-multilib whitelisted for use.
-                      if not (compiler in ["gcc-4.8", "gcc-4.9"] and
-                              bits == "32")])
+                      # XXX: 32-bit GCC 4.9 does not work because Travis does
+                      # not have g++-4.9-multilib whitelisted for use.
+                      if not (compiler == "gcc-4.9" and arch == "x86")])
                       
 entry_template = """
     - env: %(uppercase)s_VERSION=%(version)s CMAKE_BUILD_TYPE=%(mode)s BITS=%(bits)s
@@ -66,24 +68,26 @@ entry_template = """
           sources:
             %(sources)s"""
 
-def format_entry(compiler, mode, bits):
+def format_entry(compiler, mode, arch):
+    bits = 64 if arch in ["x86_64"] else 32
+
     def prefix_all(prefix, xs):
         return [prefix + x for x in xs]
 
-    packages = sorted(get_packages_to_install(compiler, bits))
+    packages = sorted(get_packages_to_install(compiler, arch))
     sources_with_dups = sum([get_sources_for_package(p) for p in packages],[])
     sources = sorted(list(set(sources_with_dups)))
     (compiler_name, compiler_version) = compiler.split("-")
     return entry_template % {
-            "uppercase" : compiler_name.upper(),
-            "version" : compiler_version,
-            "mode" : mode,
             "bits" : bits,
+            "mode" : mode,
             "packages" : "\n            ".join(prefix_all("- ", packages)),
             "sources" : "\n            ".join(prefix_all("- ", sources)),
+            "uppercase" : compiler_name.upper(),
+            "version" : compiler_version,
             }
 
-def get_packages_to_install(compiler, bits):
+def get_packages_to_install(compiler, arch):
     # clang 3.4 is already installed
     if compiler == "clang-3.4":
         packages = []
@@ -94,7 +98,7 @@ def get_packages_to_install(compiler, bits):
     else:
         raise ValueError("unexpected compiler: %s" % compiler)
         
-    if bits == "32":
+    if arch == "x86":
         if compiler.startswith("clang-"):
             packages += ["libc6-dev-i386",
                          "gcc-multilib",
@@ -105,6 +109,10 @@ def get_packages_to_install(compiler, bits):
                          "linux-libc-dev:i386"]
         else:
             raise ValueError("unexpected compiler: %s" % compiler)
+    elif arch == "x86_64":
+        pass
+    else:
+        raise ValueError("unexpected arch: %s" % arch)
 
     return packages
 
